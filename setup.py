5431305c764e31419c76e275413305ddefa88b6c
Beyang Liu <beyang.liu@gmail.com>
2014-07-11 18:45:46 +0000

    initial files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='single-module-lib',
+    version='0.0.0',
+    url='http://github.com/sgtest/python-sample-2',
+    py_modules=['mod'],
+    install_requires=[],
+)

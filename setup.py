cb3a74e9819d9e3cede98da0450f03b8ff8d89be
Beyang Liu <beyang@sourcegraph.com>
2018-02-10 22:48:36 -0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='single-module-lib',
-    version='0.0.0',
+    version='0.0.1',
     url='http://github.com/sgtest/python-single-module-lib',
     py_modules=['mod'],
     install_requires=[],
